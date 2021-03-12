5b4339f29849af16235c5164fc0d76e49cfe341b
Francesco pippi <Francescopippi02@gmail.com>
2021-03-12 13:10:30 +0100

    Creo la classe

---
+++ Pippi_AsyncSocketLib/Model/ClientChat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pippi_AsyncSocketLib.Model
+{
+    class ClientChat
+    {
+
+        public string Nickname { get; set; }
+
+        public TcpClient Client { get; set; }
+    }
+}
